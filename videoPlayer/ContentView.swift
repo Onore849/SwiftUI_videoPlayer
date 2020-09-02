91a927e31b9cc8f56b67f6d0715e0247d5c2098e
Onore849 <rion3376@gmail.com>
2020-09-02 11:36:53 +0900

    Initial Commit

---
+++ videoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  videoPlayer
+//
+//  Created by 野澤拓己 on 2020/09/02.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
